6ee137a8a407a458f5e7313e84ed3251ed4d5812
lumi <73795868+lumiafonso@users.noreply.github.com>
2022-04-02 19:01:20 +0100

    Parse working

---
+++ numbrix.py
@@ -0,0 +1,138 @@
+# numbrix.py: Template para implementação do projeto de Inteligência Artificial 2021/2022.
+# Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
+# Além das funções e classes já definidas, podem acrescentar outras que considerem pertinentes.
+
+# Grupo 052:
+# 95622 Luis Afonso
+# 95575 Francisco Goncalves
+
+import sys
+from search import Problem, Node, astar_search, breadth_first_tree_search, depth_first_tree_search, greedy_search, recursive_best_first_search
+
+matrix = []
+size = 0
+
+class NumbrixState:
+    state_id = 0
+
+    def __init__(self, board):
+        self.board = board
+        self.id = NumbrixState.state_id
+        NumbrixState.state_id += 1
+
+    def __lt__(self, other):
+        return self.id < other.id
+        
+    # TODO: outros metodos da classe
+
+
+class Board:
+    """ Representação interna de um tabuleiro de Numbrix. """
+    
+    def get_number(self, row: int, col: int) -> int:
+        """ Devolve o valor na respetiva posição do tabuleiro. """
+        return matrix[row][col]
+    
+    def adjacent_vertical_numbers(self, row: int, col: int) -> (int, int):
+        """ Devolve os valores imediatamente abaixo e acima, 
+        respectivamente. """
+        vert = []
+        global size
+        if(row+1 > size-1):
+            vert.append(None)
+        else:
+            vert.append(matrix[row+1][col])
+
+        if(row-1 < 0):
+            vert.append(None)
+        else:
+            vert.append(matrix[row-1][col])
+        
+        return vert 
+    
+    def adjacent_horizontal_numbers(self, row: int, col: int) -> (int, int):
+        """ Devolve os valores imediatamente à esquerda e à direita, 
+        respectivamente. """
+        hor = []
+        global size
+        if(col-1 < 0):
+            hor.append(None)
+        else:
+            hor.append(matrix[row][col-1])
+
+        if(col+1 > size-1):
+            hor.append(None)
+        else:
+            hor.append(matrix[row][col+1])
+        
+        return hor
+    
+    @staticmethod    
+    def parse_instance(filename: str):
+        """ Lê o ficheiro cujo caminho é passado como argumento e retorna
+        uma instância da classe Board. """
+        global size
+        f = open(filepath,"r")
+
+        with open(filepath) as f:
+            lines = f.readlines()
+        size = int(lines[0])
+
+        row = []
+        for i in range(1,size+1):
+            for j in range(len(lines[i])):
+                if(lines[i][j].isnumeric()):
+                    row.append(int(lines[i][j]))
+            matrix.append(row)
+            row = []
+
+        pass
+
+    # TODO: outros metodos da classe
+
+
+class Numbrix(Problem):
+    def __init__(self, board: Board):
+        """ O construtor especifica o estado inicial. """
+        # TODO
+        pass
+
+    def actions(self, state: NumbrixState):
+        """ Retorna uma lista de ações que podem ser executadas a
+        partir do estado passado como argumento. """
+        # TODO
+        pass
+
+    def result(self, state: NumbrixState, action):
+        """ Retorna o estado resultante de executar a 'action' sobre
+        'state' passado como argumento. A ação a executar deve ser uma
+        das presentes na lista obtida pela execução de 
+        self.actions(state). """
+        # TODO
+        pass
+
+    def goal_test(self, state: NumbrixState):
+        """ Retorna True se e só se o estado passado como argumento é
+        um estado objetivo. Deve verificar se todas as posições do tabuleiro 
+        estão preenchidas com uma sequência de números adjacentes. """
+        # TODO
+        pass
+
+    def h(self, node: Node):
+        """ Função heuristica utilizada para a procura A*. """
+        # TODO
+        pass
+    
+    # TODO: outros metodos da classe
+
+
+if __name__ == "__main__":
+    # TODO:
+    # Ler o ficheiro de input de sys.argv[1],
+    # Usar uma técnica de procura para resolver a instância,
+    # Retirar a solução a partir do nó resultante,
+    # Imprimir para o standard output no formato indicado.
+    board = Board()
+    filepath = sys.argv[1]
+    board.parse_instance(filepath)
+    pass
