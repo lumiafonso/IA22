83cea2700e7e3e8aa4aab7d1f142cbd581926693
Luis Afonso <afonso.luis2001@gmail.com>
2022-04-19 20:49:32 +0100

    major changes - not working yet

--- numbrix.py
+++ numbrix.py
@@ -9,8 +9,7 @@
 import sys
 from search import Problem, Node, astar_search, breadth_first_tree_search, depth_first_tree_search, greedy_search, recursive_best_first_search
 from copy import deepcopy
-import cProfile
-import pstats
+import time
 
 class NumbrixState:
     state_id = 0
@@ -25,14 +24,12 @@ class NumbrixState:
         
     # TODO: outros metodos da classe
 
-
 class Board:
     """ Representação interna de um tabuleiro de Numbrix. """
-    
     def __init__(self):
         self.matrix = []
         self.size = 0
-        self.used_numbers = []
+        self.missing_numbers = set()
     
     def get_number(self, row: int, col: int) -> int:
         """ Devolve o valor na respetiva posição do tabuleiro. """
@@ -41,7 +38,6 @@ class Board:
     def set_number(self, row: int, col: int, number: int):
         """ Coloca o valor na respetiva posição do tabuleiro. """
         self.matrix[row][col] = number
-        
     
     def adjacent_vertical_numbers(self, row: int, col: int) -> (int, int):
         """ Devolve os valores imediatamente abaixo e acima, 
@@ -87,6 +83,9 @@ class Board:
             lines = f.readlines()
         board.size = int(lines[0])
 
+        for i in range (1, board.size**2+1):
+            board.missing_numbers.add(i)
+
         row = []
         for i in range(1, board.size + 1):
             row = lines[i].split()
@@ -94,7 +93,7 @@ class Board:
             for n in row:
                 new_row.append(int(n))
                 if int(n) > 0:
-                    board.used_numbers.append(int(n))
+                    board.missing_numbers.remove(int(n))
             board.matrix.append(new_row)
 
         return board
@@ -123,36 +122,54 @@ class Numbrix(Problem):
         """ Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento. """
         actions_list = []
-        for i in range (state.board.size):
-            for j in range (state.board.size):
-                if state.board.get_number(i,j) == 0:
-                    hor = state.board.adjacent_horizontal_numbers(i,j)
-                    vert = state.board.adjacent_vertical_numbers(i,j)
-                    adj_list = hor+vert
-                    #se ha dois numeros consecutivos ja sabe qual valor meter
-                    value = self.two_consecutive_nums(adj_list) 
+        for missing_num in state.board.missing_numbers:
+            time.sleep(2)
+            print("missing_num:",missing_num)
+            intersection = False
+            if missing_num - 1 not in state.board.missing_numbers and missing_num -1 != 0 and missing_num + 1 not in state.board.missing_numbers:
+                intersection = True
+            if missing_num - 1 not in state.board.missing_numbers and missing_num -1 != 0:
+                #em vez de percorrer a matriz da para guardar a posicao dos numeros da board - FIX
+                temp_actions = []
+                for i in range(state.board.size):
+                    for j in range(state.board.size):
+                        if state.board.get_number(i,j) == missing_num-1:
+                            hor = state.board.adjacent_horizontal_numbers(i,j)
+                            vert = state.board.adjacent_vertical_numbers(i,j)
+                            adj_list = hor + vert
+                            temp_actions = (self.get_actions_list(state,(i,j),adj_list))
+                if intersection:
+                    actions_list.append(temp_actions)
+                    temp_actions = []
+                else:
+                    print("1",temp_actions)
+                    return temp_actions
+            if missing_num + 1 not in state.board.missing_numbers:
+                #em vez de percorrer a matriz da para guardar a posicao dos numeros da board - FIX
+                temp_actions = []
+                for i in range(state.board.size):
+                    for j in range(state.board.size):
+                        if state.board.get_number(i,j) == missing_num+1:
+                            hor = state.board.adjacent_horizontal_numbers(i,j)
+                            vert = state.board.adjacent_vertical_numbers(i,j)
+                            adj_list = hor + vert
+                            #TODO adicionar o numero as posicoes adjacentes
+                            temp_actions = (self.get_actions_list(state,(i,j),adj_list))
+                if intersection:
+                    actions_list.append(temp_actions)
+                    temp_actions = []
+                else:
+                    print("2",temp_actions)
+                    return temp_actions
+            #if the neighbours are both on the board, return the intersection of their possible actions    
+            if intersection:
+                for action in actions_list[0]:
+                    if action not in actions_list[1]:
+                        actions_list[0].remove(action)
+                print("3",actions_list[0])
+                return actions_list[0]
 
-                    if (value != 0) and (value not in state.board.used_numbers):
-                        actions_list.append((i,j,value))
-                    #se nao ha vizinhos 0, para chegar aqui quer dizer que vai ser ou 1 ou size**2
-                    #falta apagar os da actions list caso se faca um append antes do else
-                    elif (0 not in adj_list):
-                        if 2 in adj_list:
-                            if(1 not in state.board.used_numbers):
-                                actions_list.append((i,j,1))
-                        elif state.board.size**2-1 in adj_list:
-                            if(state.board.size**2 not in state.board.used_numbers):
-                                actions_list.append((i,j,state.board.size**2))
-                        return actions_list
 
-                    for x in range(1,state.board.size**2+1):
-                        if x not in state.board.used_numbers:
-                            if x+1 in adj_list or x+1 not in state.board.used_numbers:
-                                if x-1 in adj_list or x-1 not in state.board.used_numbers:
-                                    if (i,j,x) not in actions_list:
-                                        actions_list.append((i,j,x))
-                    return actions_list
-    
     def result(self, state: NumbrixState, action):
         """ Retorna o estado resultante de executar a 'action' sobre
         'state' passado como argumento. A ação a executar deve ser uma
@@ -160,19 +177,17 @@ class Numbrix(Problem):
         self.actions(state). """
 
         new_board = deepcopy(state.board)
-
-        """ print("action:",action) """
     
         old_num = new_board.get_number(action[0],action[1])
         
         if old_num != 0:
-            new_board.used_numbers.remove(old_num)
+            new_board.missing_numbers.add(old_num)
             
         new_board.set_number(action[0],action[1],action[2])
 
-        """ print("new_board\n"+new_board.to_string()) """
+        print("new_board\n"+new_board.to_string())
 
-        new_board.used_numbers.append(action[2])
+        new_board.missing_numbers.remove(action[2])
 
         return NumbrixState(new_board)
     
@@ -222,6 +237,36 @@ class Numbrix(Problem):
                     if i==j-2:
                         return i+1
         return 0
+    
+    def get_actions_list(self, state, pos, adj_list):
+        actions_list = []
+        i = pos[0]
+        j = pos[1]
+        #se ha dois numeros consecutivos ja sabe qual valor meter
+        value = self.two_consecutive_nums(adj_list)
+
+        if (value != 0) and (value in state.board.missing_numbers):
+            actions_list.append((i,j,value))
+        #se nao ha vizinhos 0, para chegar aqui quer dizer que vai ser ou 1 ou size**2
+        #falta apagar os da actions list caso se faca um append antes do else
+        elif (0 not in adj_list):
+            if 2 in adj_list:
+                if(1 in state.board.missing_numbers):
+                    actions_list.append((i,j,1))
+            elif state.board.size**2-1 in adj_list:
+                if(state.board.size**2 in state.board.missing_numbers):
+                    actions_list.append((i,j,state.board.size**2))
+                    return actions_list
+
+        for x in range(1,state.board.size**2+1):
+            if x in state.board.missing_numbers:
+                if x+1 in adj_list or x+1 in state.board.missing_numbers:
+                    if x-1 in adj_list or x-1 in state.board.missing_numbers:
+                        if (i,j,x) not in actions_list:
+                            actions_list.append((i,j,x))
+
+        """ print("actions_list:",actions_list) """
+        return actions_list
 
 
 if __name__ == "__main__":
@@ -230,16 +275,13 @@ if __name__ == "__main__":
     # Usar uma técnica de procura para resolver a instância,
     # Retirar a solução a partir do nó resultante,
     # Imprimir para o standard output no formato indicado.
-    with cProfile.Profile() as profile:
-        filepath = sys.argv[1]
-        # Ler tabuleiro do ficheiro 'i1.txt' (Figura 1):
-        board = Board.parse_instance(filepath)
-        # Criar uma instância de Numbrix:
-        problem = Numbrix(board)
-        # Obter o nó solução usando a procura:
-        goal_node = breadth_first_tree_search(problem)
-        stats = pstats.Stats(profile)
-        stats.sort_stats(pstats.SortKey.TIME)
-        stats.print_stats()
+    filepath = sys.argv[1]
+    # Ler tabuleiro do ficheiro 'i1.txt' (Figura 1):
+    board = Board.parse_instance(filepath)
+    # Criar uma instância de Numbrix:
+    problem = Numbrix(board)
+    # Obter o nó solução usando a procura:
+    goal_node = depth_first_tree_search(problem)
+
     # Verificar se foi atingida a solução
     print("Solution:\n", goal_node.state.board.to_string(), sep="")
